5cf9bd15e0c4e9f632619aca8e6fc3baf151e215
Matheus Henrique <matherique@gmail.com>
2022-08-25 21:22:36 -0300

    feat: add ex 1 lista 2

---
+++ 2022/lista-2/lista-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lista_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EX1_a.Run();
+        }
+    }
+
+    class EX1_a
+    {
+        public static void Run()
+        {
+            int n1 = 0, n2 = 0, n3 = 0;
+            
+            Console.Write("Escreva um numero: ");  
+            string n1Str = Console.ReadLine();
+            n1 = int.Parse(n1Str);
+
+            Console.Write("Escreva outro numero: ");  
+            string n2Str = Console.ReadLine();
+            n2 = int.Parse(n2Str);
+
+            Console.Write("Escreva outro numero: ");  
+            string n3Str = Console.ReadLine();
+            n3 = int.Parse(n3Str);
+
+            int[] result = new int[] {n1,n2,n3};
+
+            Array.Sort(result);
+
+            Console.WriteLine("Os numeros informados em ordem crescente: {0}, {1}, {2}", result[0], result[1], result[2]);
+        }
+    }
+}
+
